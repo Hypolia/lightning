634685b838303e835e039637b2136125cabd2046
Nathael Bonnal <kirua48000@gmail.com>
2023-05-04 01:38:00 +0200

    :construction: init types

---
+++ libs/board/filter/types/README.md
@@ -0,0 +1,11 @@
+# board-filter-types
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build board-filter-types` to build the library.
+
+## Running unit tests
+
+Run `nx test board-filter-types` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/board/filter/types/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'board-filter-types',
+  preset: '../../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../../coverage/libs/board/filter/types',
+};
