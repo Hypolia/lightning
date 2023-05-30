5a13f1f11185e38365029b3f21bd675dffcc4b68
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-05-30 18:37:54 +0200

    :construction: add `TailwindCSS`

--- apps/obsidian/src/app/app.tsx
+++ apps/obsidian/src/app/app.tsx
@@ -1,51 +1,32 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-
-import NxWelcome from './nx-welcome';
-
-import { Route, Routes, Link } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Home from "./pages";
+import {useState} from "react";
+import NavigationContext, {NavigationContract} from "./contexts/navigation-context";
+import {HomeIcon} from "@heroicons/react/24/outline";
 
 export function App() {
+  const [navigation, setNavigation] = useState<NavigationContract[]>([
+    { label: 'Home', href: '/', icon: HomeIcon as never },
+  ])
+
+  const routes = [
+    { uid: "home", href: "/", component: <Home />}
+  ]
   return (
     <div>
-      <NxWelcome title="obsidian" />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes>
-      {/* END: routes */}
+      <NavigationContext.Provider value={[navigation, setNavigation]}>
+        <Routes>
+          <Route path={"/"}>
+            { routes.map((route) => (
+              <Route
+                key={route.uid}
+                path={route.href}
+                element={route.component}
+              />
+            ))}
+          </Route>
+        </Routes>
+      </NavigationContext.Provider>
     </div>
   );
 }
