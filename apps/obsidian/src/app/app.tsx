a3844f73f44eb44a41e7eb1277b796e6b32584e0
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-06-01 10:25:56 +0200

    :construction: init AuthenticationContext

--- apps/obsidian/src/app/app.tsx
+++ apps/obsidian/src/app/app.tsx
@@ -4,11 +4,13 @@ import {useState} from "react";
 import NavigationContext, {NavigationContract} from "./contexts/navigation-context";
 import {HomeIcon} from "@heroicons/react/24/outline";
 import LoginPage from "./pages/login";
+import {AuthenticationContext} from "@lightning/shared/auth";
 
 export function App() {
   const [navigation, setNavigation] = useState<NavigationContract[]>([
     { label: 'Home', href: '/', icon: HomeIcon as never },
   ])
+  const authentication = useState()
 
   const routes = [
     { uid: "home", href: "/", component: <Home />},
@@ -16,19 +18,22 @@ export function App() {
   ]
   return (
     <div>
-      <NavigationContext.Provider value={[navigation, setNavigation]}>
-        <Routes>
-          <Route path={"/"}>
-            { routes.map((route) => (
-              <Route
-                key={route.uid}
-                path={route.href}
-                element={route.component}
-              />
-            ))}
-          </Route>
-        </Routes>
-      </NavigationContext.Provider>
+      <AuthenticationContext.Provider value={authentication as never}>
+        <NavigationContext.Provider value={[navigation, setNavigation]}>
+          <Routes>
+            <Route path={"/"}>
+              { routes.map((route) => (
+                <Route
+                  key={route.uid}
+                  path={route.href}
+                  element={route.component}
+                />
+              ))}
+            </Route>
+          </Routes>
+        </NavigationContext.Provider>
+      </AuthenticationContext.Provider>
+
     </div>
   );
 }
